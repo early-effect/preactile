b4738d618d9aefa617fd21e74afd128830b692f7
Russ White <356303+russwyte@users.noreply.github.com>
2026-09-23 10:03:32 -0500

    Compile on Scala 3.9.
    The sbt 2.1 and zipx 0.11.1 bump left the catalog on 3.8.4.

--- project/ZipxVersions.scala
+++ project/ZipxVersions.scala
@@ -8,7 +8,7 @@ import zipx.*
 object ZipxVersions extends zipx.ZipxVersions:
 
   val sbt: SbtVersion     = SbtVersion("2.1.0-M2")
-  val scala: ScalaVersion = ScalaVersion("3.8.4")
+  val scala: ScalaVersion = ScalaVersion("3.9.0")
 
   // Libraries
   val zio: Lib               = Lib("dev.zio", "zio", "2.1.26")
